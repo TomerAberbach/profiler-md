047a4a8cee5cd27247ee9db2bf310b01d474e47d
Tomer Aberbach <tomer@aberba.ch>
2026-04-12 18:32:36 -0400

    chore: add clarifying comment about `bun`

--- src/v8-heap-profile.ts
+++ src/v8-heap-profile.ts
@@ -24,6 +24,9 @@ import type {
  * You can generate a V8 heap profile in several ways:
  * - [`node --heap-prof`](https://nodejs.org/api/cli.html#--heap-prof)
  * - [`v8.writeHeapProfileSync()`](https://nodejs.org/api/v8.html#v8writeheapprofilesyncfilename)
+ *
+ * Note that [`bun --heap-prof`](https://bun.com/docs/project/benchmarking#heap-profiling)
+ * generates a heap _snapshot_ on exit, not a heap _profile_.
  */
 export const v8HeapProfileToMd = (
   text: string,
