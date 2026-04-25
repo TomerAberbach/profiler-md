4ff3cf6293eed48dfd151c96d72aaf2faa1bc825
Tomer Aberbach <tomer@aberba.ch>
2026-04-25 17:17:17 -0400

    chore: refactor benchmarking script

--- scripts/benchmarks/v8-cpu-profile.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'node:fs/promises'
-import { v8CpuProfileToMd } from '../../src/index.ts'
-import { fixturePath } from '../../src/testing/fixtures.ts'
-
-const data = await readFile(fixturePath(`example.cpuprofile`))
-
-for (let i = 0; i < 50; i++) {
-  v8CpuProfileToMd(data, { cwd: `/Users/tomer/Documents/work/code` })
-}
--- scripts/benchmarks/v8-heap-snapshot.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'node:fs/promises'
-import { v8HeapSnapshotToMd } from '../../src/index.ts'
-import { fixturePath } from '../../src/testing/fixtures.ts'
-
-const data = await readFile(fixturePath(`example.heapsnapshot`))
-
-for (let i = 0; i < 3; i++) {
-  v8HeapSnapshotToMd(data, { cwd: `/Users/tomer/Documents/work/code` })
-}
